071685962a2a4ffb7aef53c99746a79fcd7a0402
9800 <siiiiiiiib@yahoo.com>
2026-08-11 11:56:43 +0330

    MainActivity.kt

---
+++ app/src/main/java/ir/parvaz/calendar/MainActivity.kt
@@ -0,0 +1,15 @@
+package ir.parvaz.calendar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import ir.parvaz.calendar.ui.ParvazApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParvazApp()
+        }
+    }
+}
