b33f1d527a5ca99812c27dcc8111c3aea0574832
9800 <siiiiiiiib@yahoo.com>
2026-08-11 11:57:09 +0330

    Color.kt

---
+++ app/src/main/java/ir/parvaz/calendar/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package ir.parvaz.calendar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TealPrimary = Color(0xFF0F766E)
+val OnTeal = Color(0xFFFFFFFF)
+val TealContainer = Color(0xFF99F6E4)
+val OnTealContainer = Color(0xFF134E4A)
+val DeepTeal = Color(0xFF115E59)
+val WarmBackground = Color(0xFFFBFDFC)
+val DarkText = Color(0xFF1F2937)
+
+val TealLight = Color(0xFF5EEAD4)
+val TealContainerDark = Color(0xFF134E4A)
+val TealContainerText = Color(0xFFCCFBF1)
+val DarkBackground = Color(0xFF0B1220)
+val DarkSurface = Color(0xFF111827)
+val LightText = Color(0xFFF9FAFB)
