6b47275fa562681d2661a2e770dee86a07d2750e
9800 <siiiiiiiib@yahoo.com>
2026-08-17 10:10:50 +0330

    Update MainActivity.kt

--- app/src/main/java/ir/parvaz/calendar/MainActivity.kt
+++ app/src/main/java/ir/parvaz/calendar/MainActivity.kt
@@ -1,6 +1,8 @@
 package ir.parvaz.calendar
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import ir.parvaz.calendar.core.icon.IconManager
@@ -9,9 +11,15 @@ import ir.parvaz.calendar.ui.ParvazApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        IconManager.update(this)
         setContent {
             ParvazApp()
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            try {
+                IconManager.update(this)
+            } catch (exception: Exception) {
+            }
+        }, 5000)
     }
 }
