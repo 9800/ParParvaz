6af8fb75bf13e4b99c8e8b458cee259b05340438
9800 <siiiiiiiib@yahoo.com>
2026-08-17 09:19:58 +0330

    Update MainActivity.kt

--- app/src/main/java/ir/parvaz/calendar/MainActivity.kt
+++ app/src/main/java/ir/parvaz/calendar/MainActivity.kt
@@ -3,11 +3,13 @@ package ir.parvaz.calendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import ir.parvaz.calendar.core.icon.IconManager
 import ir.parvaz.calendar.ui.ParvazApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        IconManager.update(this)
         setContent {
             ParvazApp()
         }
